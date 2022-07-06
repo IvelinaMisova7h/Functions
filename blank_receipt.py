f673b7a9cf54eab9fa1689a6383371beef503580
ivelinamisova <ivelina_misova@abv.bg>
2022-07-06 21:45:19 +0300

    init

---
+++ blank_receipt.py
@@ -0,0 +1,15 @@
+def print_header():
+    print("CASH RECEIPT\n-----------------------")
+
+def print_body():
+    print("Charged to___________________\nReceived by____________________")
+
+def print_footer():
+    print("------------------\n@ SoftUni")
+
+def print_receipt():
+    print_header()
+    print_body()
+    print_footer()
+
+print_receipt()
