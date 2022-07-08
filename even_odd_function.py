126ed455c9e9c2ce0aa4e299fdb3c84536a30091
ivelinamisova <ivelina_misova@abv.bg>
2022-07-08 19:44:49 +0300

    init

---
+++ even_odd_function.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+def even_odd_function(a):
+    if a % 2 == 0:
+        print(f"The number {a} is even.")
+    else:
+        print(f"The number {a} is odd.")
+
+
+even_odd_function(n)
