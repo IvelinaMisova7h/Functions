8c33a2a5422c0686ea98d63a28805cf332e627a2
ivelinamisova <ivelina_misova@abv.bg>
2022-08-18 19:25:12 +0300

    init

---
+++ draws_a_grid.py
@@ -0,0 +1,11 @@
+def drawing_a_grid():
+    print('+' + 4*' - ' + '+' + 4*' - ' + '+')
+    for i in range(0, 4):
+        print('|' + 12*' ' + '|' + 12*' ' + '|')
+    print('+' + 4 * ' - ' + '+' + 4 * ' - ' + '+')
+    for i in range(0, 4):
+        print('|' + 12*' ' + '|' + 12*' ' + '|')
+    print('+' + 4 * ' - ' + '+' + 4 * ' - ' + '+')
+
+
+drawing_a_grid()
